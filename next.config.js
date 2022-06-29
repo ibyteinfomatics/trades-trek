e8c1fbc08b3b6e7e9e7e898900d927a50b7fe8d0
deepak <deepak@ibyteinfomatics.com>
2022-06-29 15:55:37 +0530

    config file change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportTrailingSlash: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    loader: 'imgix',
+    path: 'http://tradestrek.ibyteworkshop.com',
+  },
 }
 
 module.exports = nextConfig
